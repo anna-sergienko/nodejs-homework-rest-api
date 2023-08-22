2f3570ceacb85182eeb965c6e315693c69e82735
Anna Sergienko <annaserg02@gmail.com>
2023-08-22 18:01:39 +0300

    done

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,10 +3,13 @@ const isValidId = require("./isValidId");
 const checkBody = require("./checkBody");
 const checkFavorite = require("./checkFavorite");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
+
 module.exports = {
   validateBody,
   isValidId,
   checkBody,
   checkFavorite,
   authenticate,
+  upload
 };
